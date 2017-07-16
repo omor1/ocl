b421115d3c815a13016d87ecfe1bfd1b8d7efd62
Omri Mor <omri50@gmail.com>
2017-07-15 21:43:53 -0700

    Formatting

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    std::cout << "OpenCL Wrapper Comming Soon!" << std::endl;
 
-  std::cout<<"OpenCL Wrapper Comming Soon!"<<std::endl;  
-  
-  return 0;
+    return 0;
 }
